220f1b037260e92088a1e97b05aa65c018985ecf
Anastasiia Bakai <anastasiia.bakai@gmail.com>
2017-08-15 18:36:08 +0400

    cookies

--- dist/assets/js/base.js
+++ dist/assets/js/base.js
@@ -7695,14 +7695,86 @@ jQuery(document).ready(function($){
 
 
     /*====== PRINT ======*/
-    $('.card-print').on('click', function(e){
-        e.preventDefault();
-        e.stopPropagation();
+    if($('.card-print').length > 0){
+        $('.card-print').on('click', function(e){
+            e.preventDefault();
+            e.stopPropagation();
 
-        $(this).parents('section.sec').addClass('print');
-        window.print();
-        $(this).parents('section.sec').removeClass('print');
-    });
+            $(this).parents('section.sec').addClass('print');
+            window.print();
+            $(this).parents('section.sec').removeClass('print');
+        });
+    }
 
+
+
+    /*====== TEXT RESIZE ======*/
+
+    function textsize_scale(i){
+
+        $('.js-tsize-plus').removeClass('active');
+        $('.js-tsize-plus span').html('');
+        $('.js-tsize-minus').removeClass('active');
+        $('.js-tsize-minus span').html('');
+
+        if ( i > 0 ){
+            $('.js-tsize-plus').each(function(){
+                $(this).addClass('active');
+                $(this).find('span').html(i+1);
+            });
+        }
+
+        if ( i < 0 ){
+            $('.js-tsize-minus').each(function(){
+                $(this).addClass('active');
+                $(this).find('span').html(i-1);
+            });
+        }
+    }
+
+    function textsize_cookies_get(){
+        if ( Cookies.get('textsize') == 1)
+            return Cookies.get('textsize');
+        else
+            return 0;
+    }
+
+    function textsize_cookies_set(i){
+        Cookies.set('textsize', i, { expires: 7 });
+        console.log(Cookies.get('textsize'));
+    }
+
+
+    if($('.js-tsize-plus').length > 0){
+        var scale = 100;
+        var i = textsize_cookies_get();
+
+
+        $('.js-tsize-plus').on('click', function(e){
+            e.preventDefault();
+            e.stopPropagation();
+
+            scale += 5;
+            i++;
+
+            textsize_scale(i);
+            textsize_cookies_set(i);
+            $('html').css('font-size', scale + '%');
+        });
+
+
+
+        $('.js-tsize-minus').on('click', function(e){
+            e.preventDefault();
+            e.stopPropagation();
+
+            scale -= 5;
+            i--;
+
+            textsize_scale(i);
+            textsize_cookies_set(i);
+            $('html').css('font-size', scale + '%');
+        });
+    }
 
 });
